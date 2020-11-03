a512fa3aab3aa900e80ea97314f3c7feef82a509
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-03 14:38:37 +0530

    templates and assets updated

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+
+    dest1 = Destination()
+    return render(request, "index.html", {"dest1": dest1})
