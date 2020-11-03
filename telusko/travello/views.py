f6a768e89858343559d59d01fe97f57b13c3d3e2
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-03 14:44:47 +0530

    Single destination view done

--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -7,4 +7,7 @@ from .models import Destination
 def index(request):
 
     dest1 = Destination()
+    dest1.name = "Mumbai"
+    dest1.desc = "The city that never sleeps"
+    dest1.price = 700
     return render(request, "index.html", {"dest1": dest1})
