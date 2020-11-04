19771862c1939cb80b5831c75ed24fde4e99ae21
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-04 23:00:59 +0530

    connect pgsql and created model

--- telusko/telusko/settings.py
+++ telusko/telusko/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD': "123456",
+        "HOST": "localhost"
     }
 }
 
