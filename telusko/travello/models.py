e339b8d998a70652d2cb19703644d5a7dec63ba3
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-05 20:57:58 +0530

    added media directory for images

--- telusko/travello/models.py
+++ telusko/travello/models.py
@@ -9,3 +9,6 @@ class Destination(models.Model):
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- telusko/travello/views.py
+++ telusko/travello/views.py
@@ -6,27 +6,6 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = "Mumbai"
-    dest1.desc = "The city that never sleeps"
-    dest1.img = "destination_1.jpg"
-    dest1.price = 700
-    dest1.offer = False
-
-    dest2 = Destination()
-    dest2.name = "Hyderabad"
-    dest2.desc = "First biryani"
-    dest2.img = "destination_2.jpg"
-    dest2.price = 650
-    dest2.offer = True
-
-    dest3 = Destination()
-    dest3.name = "Bangalore"
-    dest3.desc = "IT City"
-    dest3.img = "destination_3.jpg"
-    dest3.price = 750
-    dest3.offer = True
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, "index.html", {"dests": dests})
