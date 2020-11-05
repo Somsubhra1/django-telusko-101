ef31aadfd499dd21f593541918a55d6de31a07e0
Somsubhra Das <das.somsubhra1@gmail.com>
2020-11-05 23:13:40 +0530

    user registration form done

--- telusko/accounts/urls.py
+++ telusko/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "accounts"
+
 urlpatterns = [
     path("register/", views.register, name="register"),
 ]
--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 == password2:
+
+            if User.objects.filter(username=username).exists():
+                print("Username taken")
+
+            elif User.objects.filter(email=email).exists():
+                print("Email already taken")
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print("user created")
+
+        else:
+            print("Password not matching")
+
+        return redirect("/")
+    else:
+        return render(request, "register.html")
